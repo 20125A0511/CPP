d34767103d800731fa506d7d94433870666e1c18
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-14 12:05:40 +0530

    -

---
+++ C++ Programs/New folder (15)/Untitled4.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int n,i;
+cout<<"Enter Array Size:";
+cin>>n;
+int a[n],sum,q,b[n];
+cout<<"Enter Array elements:";
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+cout<<"Enter Sum value:";
+cin>>sum;
+for(i=0;i<n;i++)
+{
+sum=sum-a[i];
+for(q=0;q<n;q++)
+{
+
+
+a[i]=b[q];
+}
+if(sum==0){
+cout<<"Sum of numbers between \n"<<q<<"and \n"<<i<<"\n is equal to sum value";
+break;
+}
+continue;
+}
+}
