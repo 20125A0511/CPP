981c3d4e393a4f614f96a7767d3c3ebde2444f30
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-15 09:38:52 +0530

    String Concatination

---
+++ C++ Programs/New folder (25)/Untitled3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+//String Concatination
+string str1,str2,str3;
+cout<<"Enter 1st String:";
+cin>>str1;
+cout<<"Enter 2st String:";
+cin>>str2;
+cout<<"Before Concatination :"<<str1<<"\n"<<str2<<endl;
+cout<<"After Concatination :";
+str3=str1+str2;
+cout<<str3;
+}
