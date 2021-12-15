0fd5f59f3b8b4c162cc083094764c658bcf80fe6
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-15 10:08:06 +0530

    -

---
+++ C++ Programs/Untitled5.cpp
@@ -0,0 +1,26 @@
+#include<cstring>
+#include <iostream>
+using namespace std;
+bool isAnBn(string str)
+{
+int n = str.length();
+int i;
+for (i = 0; i < n; i++)
+if (str[i] != 'x')
+break;
+if (i * 2 != n)
+return false;
+int j;
+for (j = i; j < n; j++)
+if (str[j] != 'y')
+return false;
+return true;
+}
+int main()
+{
+cout<<"Enter String:";
+string str;
+cin>>str;
+// Function call
+isAnBn(str) ? cout << "Yes" : cout << "No";
+}
