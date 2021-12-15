4057edd5f1cc448953ff36c82b24529f178786cf
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-15 10:08:14 +0530

    -

---
+++ C++ Programs/New folder (26)/Untitled4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+main()
+{
+string a,b,c,i;
+cin>>a;
+cin>>b;
+if(b.length()==a.length())
+{
+cout<<"1";
+
+}
+else
+{
+
+    cout<<"0";
+}
+
+
+}
