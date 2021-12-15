5f523319600c503bd72c53d6a4306f1625599aab
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-15 09:26:24 +0530

    Strings

---
+++ C++ Programs/New folder (24)/Untitled2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+main()
+{
+    int n,i;
+    cout<<"Enter String Size:";
+    cin>>n;
+string str[n],str;
+cout<<"Enter Strings:";
+    for(i=0;i<n;i++)
+        {
+        cin>>str[i];
+    }
+    cout<<"Entered String is:";
+    for(i=0;i<n;i++)
+        {
+        cout<<"\n"<<str[i];
+    }
+}
