85d471795a5e4b25f756271b871875209a468cde
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-17 14:35:24 +0530

    -

---
+++ C++ Programs/New folder (54)/Untitled5.cpp
@@ -0,0 +1,30 @@
+//Sets of prime number
+#include<bits/stdc++.h>
+using namespace std;
+prime(int a)
+{
+int res;
+int flag=0;
+int i;
+int m=a/2;
+for(i = 2; i <= m; i++)
+{
+if(a % i == 0)
+{
+cout<<"Number is not Prime."<<endl;
+flag=1;
+break;
+}
+}
+for (int i = 2; i <= a; i++) {
+prime(a);
+            cout << i << " ";
+    }
+
+}
+main()
+{
+    int n;
+    cin>>n;
+    prime(n);
+}
