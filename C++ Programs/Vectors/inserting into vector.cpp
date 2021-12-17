3e4f5a098fbb4c0bcb7fb27d228629d7356a809d
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-17 15:56:40 +0530

    -

--- C++ Programs/Vectors/inserting into vector.cpp
+++ C++ Programs/Vectors/inserting into vector.cpp
@@ -1,8 +1,9 @@
+//Vectors Printing and Sorting
 #include<bits/stdc++.h>
 using namespace std;
 main()
 {
-vector <int> v={9,8,7,6,5,4,3,2,1};
+vector <int> v={99,88,77,66,55,44,33,22,11};
 vector <int> :: iterator itr;
 cout<<"Vector Elements:"<<endl;
 for(itr=v.begin();itr!=v.end();itr++)
@@ -13,6 +14,4 @@ cout<<"------------------------------"<<endl;
 sort(v.begin(),v.end());
 for(itr=v.begin();itr!=v.end();itr++)
 cout<<(*itr)<<endl;
-
-
 }
