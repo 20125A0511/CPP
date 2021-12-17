761567be96cf55fb4fa8a03e3b5a82d090980909
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-17 11:44:33 +0530

    -

---
+++ C++ Programs/New folder (50)/Untitled1.cpp
@@ -0,0 +1,14 @@
+//Array Elements Starting here from 1st index ie, a[n+1]
+#include<bits/stdc++.h>
+using namespace std;
+main()
+{
+int n;
+cin>>n;
+int a[n+1];
+for(int i=1;i<n+1;i++)
+{
+cin>>a[i];
+}
+cout<<a[0];
+}
