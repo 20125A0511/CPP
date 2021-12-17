06706a25db956c6fdb0a0614ec6583313de2fd05
Charan <69421862+20125A0511@users.noreply.github.com>
2021-12-17 16:38:19 +0530

    -

--- C++ Programs/New folder (55)/Untitled11.cpp
+++ C++ Programs/New folder (55)/Untitled11.cpp
@@ -5,35 +5,34 @@ using namespace std;
 
 void find_all_triplets(int arr[], int n, int sum)
 {
-for (int i = 0; i < n – 2; i++)
+for(int i = 0; i <n-2;i++)
 {
-for (int j = i + 1; j < n – 1; j++)
+for(int j = i + 1; j < n – 1; j++)
 {
 for (int k = j + 1; k < n; k++)
 {
 if (arr[i] + arr[j] + arr[k] == sum)
 {
-cout << arr[i] << “,” << arr[j] << “,” << arr[k] <<endl;
+cout << arr[i] << ","<< arr[j] << "," << arr[k] <<endl;
 }
 }
 }
 }
 }
-
 int main()
 {
 int n, sum;
-cout << “\nEnter the number of elements : “;
+cout << "\nEnter the number of elements : ";
 cin >> n;
 int arr[n];
-cout << “\nInput the array elements : “;
+cout << "\nInput the array elements : ";
 for(int i = 0; i < n; i++)
 {
 cin >> arr[i];
 }
-cout << “\nEnter the sum value : “;
+cout << "\nEnter the sum value : ";
 cin >> sum;
-cout << “\nThe triplets are \n “;
+cout << "\nThe triplets are \n";
 find_all_triplets(arr, n, sum);
 return 0;
 }
